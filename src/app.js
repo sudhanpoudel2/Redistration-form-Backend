9126d628c55a9f921daf28fe81628b66032459be
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2023-12-27 22:09:21 +0545

    using hbs(handle bars)

--- src/app.js
+++ src/app.js
@@ -2,21 +2,25 @@
 
 import express from "express";
 import './DB/db.js'
+import path from 'path';
+// import exphbs from 'express-handlebars';
 
+// const template_path = path.join(__dirname , '../template/src')
 
-import path from 'express';
+const template_path = path.join(new URL('.', import.meta.url).pathname, "../template/views");
 const app = express();
 
-// const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', async(req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
-});
+// app.engine('handlebars', exphbs());
+app.set('view engine','handlebars');
+app.set('views',template_path)
 
 // app.get('/' ,(req,res) =>{
 //     res.send  ("Hello World! I am broken")
 // })
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log(`Listning to the port ${process.env.PORT || 3000}`);
